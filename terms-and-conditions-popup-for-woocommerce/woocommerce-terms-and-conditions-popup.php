3edcd4205b02c0fd22c4a5dad28a52ed8f16337c
Sean Murphy <sean@iamseanmurphy.com>
2025-05-14 15:17:40 +0000

    3.6.2

--- terms-and-conditions-popup-for-woocommerce/woocommerce-terms-and-conditions-popup.php
+++ terms-and-conditions-popup-for-woocommerce/woocommerce-terms-and-conditions-popup.php
@@ -3,17 +3,17 @@
  * Plugin Name: Terms and Conditions Popup for WooCommerce
  * Plugin URI: https://wordpress.org/plugins/terms-and-conditions-popup-for-woocommerce/?utm_source=free_plugin&utm_medium=plugins&utm_campaign=terms_cond_popup
  * Description: Allows your customers to see the terms and conditions without leaving the checkout page.
- * Version: 3.6.1
+ * Version: 3.6.2
  * Author: BeRocket
  * Requires at least: 5.0
- * Tested up to: 6.7
+ * Tested up to: 6.8
  * Requires PHP: 7.0
  * Requires Plugins: woocommerce
  * Author URI: https://berocket.com?utm_source=free_plugin&utm_medium=plugins&utm_campaign=terms_cond_popup
  * Text Domain: terms-and-conditions-popup-for-woocommerce
  * Domain Path: /languages/
- * WC tested up to: 9.5
+ * WC tested up to: 9.8
  */
-define( "BeRocket_terms_cond_popup_version", '3.6.1' );
+define( "BeRocket_terms_cond_popup_version", '3.6.2' );
 define( "BeRocket_terms_cond_popup_file", __FILE__ );
 include_once('main.php');
